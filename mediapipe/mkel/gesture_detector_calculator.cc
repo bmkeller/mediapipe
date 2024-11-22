04d2e26ff862123b58afb787ce0aab2783c6b7f1
Michael Keller <bmkeller@users.noreply.github.com>
2024-11-22 13:56:00 -0800

    Adding initial gesture detection

--- mediapipe/mkel/gesture_detector_calculator.cc
+++ mediapipe/mkel/gesture_detector_calculator.cc
@@ -15,89 +15,7 @@ constexpr float kTouchDistanceThreshold = 0.10f;
 constexpr char kProtoPath[] = "/tmp/proto_ipc";
 constexpr int kMaxMessagesToKeep = 20;
 
-enum class GestureTypes {
-  UNKNOWN,
-  CLICK,
-};
-
-float ComputeDistance(float dx, float dy) {
-  float d = dx * dx + dy * dy;
-
-  if (d != 0) {
-    d = sqrt(d);
-  }
-
-  return d;
-}
-
 }  // namespace
-
-struct HandState {
-  NormalizedLandmarkList landmarks;
-  std::string hand;
-  std::unordered_map<LandmarkType, NormalizedLandmark> landmark_map;
-  float min_x = std::numeric_limits<float>::max();
-  float min_y = std::numeric_limits<float>::max();
-  float max_x = std::numeric_limits<float>::min();
-  float max_y = std::numeric_limits<float>::min();
-
-  void InitializeLandmarkMap(const NormalizedLandmarkList &landmarks_list) {
-    landmarks = landmarks_list;
-
-    for (int i = kFirstLandmarkIndex; i <= kLastLandmarkIndex; i++) {
-      if (landmarks.landmark_size() > i) {
-        const auto &landmark = landmarks.landmark(i);
-        landmark_map[static_cast<LandmarkType>(i)] = landmark;
-
-        min_x = std::min(min_x, landmark.x());
-        min_y = std::min(min_y, landmark.y());
-        max_x = std::max(max_x, landmark.x());
-        max_y = std::max(max_y, landmark.y());
-      }
-    }
-  }
-
-  std::string DebugStriong(LandmarkType landmark_type) const {
-    std::stringstream ss;
-    ss << "Hand: " << hand << " " << static_cast<int>(landmark_type) << ": "
-       << landmark_map.at(landmark_type).DebugString();
-    return ss.str();
-  }
-
-  void PrintPoint(LandmarkType l) const {
-    const auto &point = landmark_map.at(l);
-    std::cout << "Point: " << point.x() << ", " << point.y() << std::endl;
-  }
-
-  float Distance(LandmarkType l1, LandmarkType l2, bool scaled) const {
-    float dx = landmark_map.at(l1).x() - landmark_map.at(l2).x();
-    float dy = landmark_map.at(l1).y() - landmark_map.at(l2).y();
-
-    if (scaled) {
-      dx /= (max_x - min_x);
-      dy /= (max_y - min_y);
-    }
-
-    return ComputeDistance(dx, dy);
-  }
-};
-
-GestureTypes GetGestureType(const HandState &hand_state) {
-  const float touchDistance = hand_state.Distance(
-      LandmarkType::THUMB_TIP, LandmarkType::INDEX_FINGER_TIP, true);
-
-  const float pinkyDistance = hand_state.Distance(
-      LandmarkType::PINKY_TIP, LandmarkType::INDEX_FINGER_TIP, true);
-
-  std::cout << "Touch distance: " << touchDistance
-            << ", Pinky distance: " << pinkyDistance << std::endl;
-
-  if (touchDistance < 0.1f && pinkyDistance > 0.2f) {
-    return GestureTypes::CLICK;
-  }
-
-  return GestureTypes::UNKNOWN;
-}
 
 absl::Status GestureRecognizerCalculator::GetContract(CalculatorContract *cc) {
   cc->Inputs()
@@ -123,10 +41,10 @@ absl::Status GestureRecognizerCalculator::Process(CalculatorContext *cc) {
     return absl::OkStatus();
   }
 
-  gesture_.set_index(frame_index_);
+  gesture_.set_index(123);
   gesture_.set_frame_index(frame_index_);
-  gesture_.set_center_x(0.0f);
-  gesture_.set_center_y(0.0f);
+  gesture_.set_center_x(2.0f);
+  gesture_.set_center_y(4.0f);
   gesture_.set_gesture_type(gestures::GestureType::NONE);
 
   // RET_CHECK(!cc->Inputs().Tag(kImageSizeTag).IsEmpty());
@@ -152,22 +70,31 @@ absl::Status GestureRecognizerCalculator::Process(CalculatorContext *cc) {
   // std::cout << "Handedness: " << handedness[0].classification(0).label()
   //           << std::endl;
 
-  HandState hand_state;
-  hand_state.InitializeLandmarkMap(landmarks[0]);
+  // HandState hand_state;
+  // hand_state.InitializeLandmarkMap(landmarks[0]);
 
-  const float touchDistance = hand_state.Distance(
-      LandmarkType::THUMB_TIP, LandmarkType::INDEX_FINGER_TIP, true);
+  // const float touchDistance = hand_state.Distance(
+  //     LandmarkType::THUMB_TIP, LandmarkType::INDEX_FINGER_TIP, true);
 
-  const bool touchDetected = touchDistance < kTouchDistanceThreshold;
+  // const bool touchDetected = touchDistance < kTouchDistanceThreshold;
 
-  if (touchDetected) {
-    OnGestureDetected();
-  }
+  // if (touchDetected) {
+  //   OnGestureDetected();
+  // }
 
-  std::cout << "Distance: " << touchDistance
-            << ", touch detected: " << touchDetected << std::endl;
+  // std::cout << "Distance: " << touchDistance
+  //           << ", touch detected: " << touchDetected << std::endl;
 
-  proto_writer_.Write(gesture_.SerializeAsString());
+  // gesture_.set_blah("this is a long test string");
+
+  gesture_detection::HandPositions handResult =
+      gesture_detector_.updateLeftHand(landmarks[0]);
+
+  std::string serialized_gesture = gesture_.SerializeAsString();
+  // std::cout << "Serialized gesture: " << serialized_gesture.size() <<
+  // std::endl;
+
+  proto_writer_.Write(serialized_gesture);
 
   return absl::OkStatus();
 }
--- mediapipe/mkel/gesture_detector_calculator.h
+++ mediapipe/mkel/gesture_detector_calculator.h
@@ -5,9 +5,9 @@
 #include "mediapipe/framework/formats/landmark.pb.h"
 #include "mediapipe/framework/port/ret_check.h"
 #include "mediapipe/framework/port/status.h"
-#include "mediapipe/mkel/hand_landmarks.h"
-
+#include "mediapipe/mkel/gesture_detector.h"
 #include "mediapipe/mkel/gestures.pb.h"
+#include "mediapipe/mkel/hand_landmarks.h"
 #include "mediapipe/mkel/proto_writer.h"
 
 namespace mediapipe {
@@ -23,10 +23,11 @@ class GestureRecognizerCalculator : public CalculatorBase {
   // Triggers when a gesture is detected.
   void OnGestureDetected();
 
-  private:
-    gestures::Gestures gesture_;
-    gestures::ProtoWriter proto_writer_;
-    int frame_index_ = 0;
+ private:
+  gestures::Gestures gesture_;
+  gestures::ProtoWriter proto_writer_;
+  gesture_detection::GestureDetector gesture_detector_;
+  int frame_index_ = 0;
 };
 
 }  // namespace mediapipe
